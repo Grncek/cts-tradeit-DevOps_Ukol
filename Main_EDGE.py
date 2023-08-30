9abfcbe3693571f9d672811be93c2fdf0975bc12
Grncek <DominikG@seznam.cz>
2023-08-30 15:12:31 +0200

    INIT commit

---
+++ Main_EDGE.py
@@ -0,0 +1,39 @@
+import re
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.edge.service import Service as EdgeService
+from webdriver_manager.microsoft import EdgeChromiumDriverManager
+
+
+def create_files_from_web(index):
+    #elements and offers are the same however "elements" become stale after first loop - variable must be declared every loop
+    offers = driver.find_elements(by=By.CSS_SELECTOR, value='[class^="card card"]')
+    card = offers[index]
+    card.click()
+    header = driver.find_element(by=By.CSS_SELECTOR, value='h1.mb-1')
+    story_text = driver.find_element(by=By.CSS_SELECTOR, value='div.story__text')
+    clean_header_text = re.sub(r'[^\w\s]', '', header.text).replace(' ', '_')
+    file_name = f"{clean_header_text}.txt"
+    with open(file_name, "w") as file:
+        file.write(story_text.text)
+    driver.back()
+
+#Start webpage
+driver = webdriver.Edge(service=EdgeService(EdgeChromiumDriverManager().install()))
+driver.get("https://www.cts-tradeit.cz/kariera/")
+
+#Get rid of cookies
+button = driver.find_element(by=By.ID, value="c-p-bn")
+button.click()
+
+elements = driver.find_elements(by=By.CSS_SELECTOR, value='[class^="card card"]')
+
+#loop through all offers
+for index, value in enumerate(elements):
+    create_files_from_web(index)
+
+
+driver.close()
+
+
+
--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # cts-tradeit-DevOps_Ukol
+I got 2 script one for Google chrome that use ChromeDrive that must be downloaded. Second use microsoft edge webdriver-manager. 
+
+Before executing Main_EDGE:
+1) Install Selenium: pip install selenium
+2) Install webdriver-manager: pip install webdriver-manager
+
+
+
+Before executing Main_CHROME:
+1) The script utilize Selenium library so you must install Selenium: pip install selenium
+2) Selenium does not work without driver so its essential to put your drive in path "C:\Program Files (x86)\chromedriver"
+3) Download ChromeDriver here: https://sites.google.com/chromium.org/driver/downloads?authuser=0
+   if you have newer version of Chrome here: https://googlechromelabs.github.io/chrome-for-testing/
+
